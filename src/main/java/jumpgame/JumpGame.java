aed4d03d4ae7582ffe9c44559ab46080a6d3dcb3
Mehak Adlakha <adlakhamehak98@gmail.com>
2023-04-25 01:04:03 +0530

    jump game

---
+++ src/main/java/jumpgame/JumpGame.java
@@ -0,0 +1,26 @@
+package main.java.jumpgame;
+
+class JumpGame {
+
+    //https://leetcode.com/problems/jump-game/
+    public boolean canJump(int[] nums) {
+        if (nums.length <= 1) {
+            return true;
+        }
+
+        int max = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (max <= i && nums[i] == 0) {
+                return false;
+            }
+            if (i + nums[i] > max) {
+                max = i + nums[i];
+            }
+            if (max >= nums.length - 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
